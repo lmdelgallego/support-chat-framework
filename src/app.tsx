735f9f3ec29f456e18a88a9b2c9492de006a964c
Luis Miguel Del Gallego Horta <luis.delgallego@licify.co>
2025-04-20 17:05:29 -0500

    First INIT

---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+import './app.css'
+import AuthLayout from './auth/layout/auth-layout'
+import ChatLayout from './chat/layout/chat-layout'
+import ChatPage from './chat/pages/chat-page'
+
+function App() {
+  return (
+    <>
+      {/* <AuthLayout /> */}
+      <ChatLayout><ChatPage/></ChatLayout>
+    </>
+  )
+}
+
+export default App

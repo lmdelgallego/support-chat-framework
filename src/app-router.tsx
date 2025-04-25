f44353f60f5bdeb0864e7a634158f82881b4eaa8
alucardluis <alucardluis@gmail.com>
2025-04-25 03:31:58 +0000

    Mock authentication

--- src/app-router.tsx
+++ src/app-router.tsx
@@ -5,6 +5,8 @@ import { LoginPage } from "./auth/pages/login-page";
 import { RegisterPage } from "./auth/pages/register-page";
 import { sleep } from "./lib/sleep";
 import PrivateRoute from "./auth/components/private-route";
+import { useQuery } from "@tanstack/react-query";
+import { checkAuth } from "./fake/fake-dta";
 
 const ChatLayout = lazy(async () => {
   await sleep(1000);
@@ -15,6 +17,32 @@ const ChatPage = lazy(() => import("./chat/pages/chat-page"));
 const NoChatSelectedPage = lazy(() => import("./chat/pages/no-chat-selected-page"));
 
 export const AppRouter = () => {
+
+  const { data: user, isLoading, isError, error } = useQuery({
+    queryKey: ['user'],
+    queryFn: () => {
+      const token = localStorage.getItem('token');
+      if(!token) throw new Error('Not authenticated');
+
+      return checkAuth(token)
+    },
+    retry: 0
+  });
+
+
+  if (isLoading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <div className="h-8 w-8 animate-spin rounded-full border-4 border-primary border-t-transparent" />
+      </div>
+    );
+  }
+
+  // if (isError) {
+  //   console.log(error)
+  //   return <div>Error: {error.message}</div>;
+  // }
+
   return (
     <BrowserRouter>
       <Routes>
@@ -33,7 +61,7 @@ export const AppRouter = () => {
                 </div>
               }
             >
-              <PrivateRoute isAuthenticated={true}>
+              <PrivateRoute isAuthenticated={!!user}>
                 <ChatLayout />
               </PrivateRoute>
             </Suspense>
